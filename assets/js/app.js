dbf4581b4e67d646b26d06487a00b235dd4afac0
MarioBroi <inoix16@gmail.com>
2024-03-14 08:40:58 +0000

    add input and button to print a new class

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,7 @@ createApp({
     data() {
         return {
             //message: 'Its working',
+            newTask: '',
             tasks: [
                 {
                     text: 'First task',
@@ -24,7 +25,14 @@ createApp({
     methods: {
         removeTask(taskId) {
             console.log('removing the task');
-            this.tasks.splice(taskId, 1)
+            this.tasks.splice(taskId, 1);
+        },
+        addTask() {
+            console.log('adding new task');
+            if (this.newTask.trim().length > 3) {
+                this.tasks.push({ text: this.newTask, done: false });
+            }
+            this.newTask.text = '';
         }
     }
 }).mount('#app');
